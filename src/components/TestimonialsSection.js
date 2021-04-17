6523407598b7ca02b306f1c08c84f76ff693d290
Stackbit Code Editor <projects@stackbit.com>
2021-04-17 02:47:10 +0000

    TestimonialsSection.js: changed

--- src/components/TestimonialsSection.js
+++ src/components/TestimonialsSection.js
@@ -14,8 +14,9 @@ export default class TestimonialsSection extends React.Component {
                         <svg className="quotes__icon" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path d="M0 14.182l4.571-14.182h4.952l-3.81 13.289h3.81v10.711h-9.524v-9.818zM14.476 14.182l4.571-14.182h4.952l-3.81 13.289h3.81v10.711h-9.524v-9.818z" />
                         </svg>
-                        <div className="quotes__text">{_.get(testimonial, 'text', null)}</div>
-                        <div className="quotes__author">{_.get(testimonial, 'author.name', null)}<span className="quotes__location">, {_.get(testimonial, 'author.location', null)}</span></div>
+                        <script type="text/javascript" src="https://testimonial.to/js/iframeResizer.min.js"></script>
+<iframe id="testimonialto-makers-rocket-dark" src="https://embed.testimonial.to/w/makers-rocket?theme=dark&card=base" frameborder="0" scrolling="no" width="100%"></iframe>
+<script type="text/javascript">"#testimonialto-makers-rocket-dark");</script>
                     </div>
                     <div key={testimonial_idx + '.1'} className="quotes__separator" />
                     </React.Fragment>))}
